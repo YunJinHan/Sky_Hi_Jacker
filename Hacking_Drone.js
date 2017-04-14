25e4b2dcadfe3a7938c680a0d5090da5de3ee936
YunJinHan <yjh9234@gmail.com>
2017-04-15 00:15:53 +0900

    Hacking Parrot and Send Parrot to GPS waypoint based on ar-drone module

--- Hacking_Drone.js
+++ Hacking_Drone.js
@@ -1,89 +1,88 @@
-var fs = require('fs');
-var util = require('util');
-var timer = require('timers');
+var fs = require('fs'),
+    util = require('util'),
+    timer = require('timers');
 // using include to make gps log file
 
-var arDrone = require('./Hacking_Drone_Module');
-var client  = arDrone.createClient();
+var arDrone = require('./Hacking_Drone_Module'),
+    parrot  = arDrone.createClient();
 
-var gps_origin_file = 'gps.json';
-// define file name
+var gps_origin_file = 'gps.json',
+    // define file name
+    origin_data_file = fs.createWriteStream(gps_origin_file, {flags : 'w'}),
+    log_stdout = process.stdout;
+    // gps log file
 
-var origin_data_file = fs.createWriteStream(gps_origin_file, {flags : 'w'});
-var log_stdout = process.stdout;
-// gps log file
+var isHack = false,
+    current_degree = 0,
+    current_latitude = 0,
+    current_longtitude = 0,
+    dest_distance = 0,
+    dest_degree = 0,
+    dest_latitude = 0,
+    dest_longtitude = 0,
+    angle_Cycle_Between = 0,
+    angle_CounterCycle_Between = 0;
 
-var isHack = false;
-
-var current_degree = 0;
-var current_latitude = 0;
-var current_longtitude = 0;
-
-var dest_distance = 0;
-
-var dest_degree = 0;
-var dest_latitude = 0;
-var dest_longtitude = 0;
-
-var angle_Cycle_Between = 0;
-var angle_CounterCycle_Between = 0;
-
-client.config('general:navdata_options',777060865);
+parrot.config('general:navdata_options',777060865);
 // turn on only gps options
 
-client.createRepl();
+parrot.createRepl();
 // PoketMon Drone Start
 
 setInterval(function() {
 
     if (isHack) { // 해당 좌료로 이동 시작
 
-        var final_break = false;
-        var cycleDirection = true;
+        let final_break = false,
+            cycleDirection = true;
 
-        while (1) {
+        while (true) {
             
             if (current_degree >= dest_degree) {
                 angle_Cycle_Between = 360 - current_degree + dest_degree;
                 angle_CounterCycle_Between = current_degree - dest_degree;
-            } // 목적지 방위각이 더 작을때
-            else {
+                // 목적지 방위각이 더 작을때
+            } else {
                 angle_Cycle_Between = dest_degree - current_degree;
                 angle_CounterCycle_Between = 360 + current_degree - dest_degree;
-            } // 목적지 방위각이 더 클때
+                // 목적지 방위각이 더 클때
+            }
 
             cycleDirection = angle_Cycle_Between > angle_CounterCycle_Between ? false : true ;
 
             while (current_degree > dest_degree + 2 || current_degree < dest_degree - 2) {
+
                 if (cycleDirection) {
-                    client.clockwise(0.1);
+                    parrot.clockwise(0.1);
+                } else {
+                    parrot.counterClockwise(0.1);
                 }
-                else {
-                    client.counterClockwise(0.1);
-                }
+
             }
 
-            client.stop(); // 회전 멈춰랏
+            parrot.stop(); // 회전 멈춰랏
 
             while (current_degree <= dest_degree + 2 && current_degree >= dest_degree - 2) {
 
-                client.front(1); // 이동 ~
+                parrot.front(1); // 이동 ~
 
-                if ((current_latitude <= dest_latitude && current_latitude >= dest_latitude) 
+                if ( (current_latitude <= dest_latitude + 0.000015 && current_latitude >= dest_latitude - 0.000015) 
                     && 
-                    (current_longtitude <= dest_longtitude && current_longtitude >= dest_longtitude)) {
+                    (current_longtitude <= dest_longtitude + 0.000015 && current_longtitude >= dest_longtitude - 0.000015) ) {
                     
-                    client.stop();
-                    client.land();
+                    // 0.00001 도 차이 = 약 1m 차이
+
+                    parrot.stop();
+                    parrot.land();
 
                     final_break = true;
-                    client._hack = false;
+                    parrot._hack = false;
                     isHack = false;
                     break;
                 }
             }
 
-            client.stop(); // 가는거랏멈춰랏
+            parrot.stop(); // 가는거 멈춰랏
 
             if (final_break) {
                 break;
@@ -94,61 +93,61 @@ setInterval(function() {
 
 });
 
-var i = 0;
-
 setInterval(function(){
 
-	client.on('navdata', function(navdata) {
+	parrot.on('navdata', function(navdata) {
 		origin_data_file = fs.createWriteStream(gps_origin_file, {flags : 'w'});
-		let data_content = util.format(navdata.gps);
-		let text = data_content.replace(/[\']+/g,'');
+
+		let data_content = util.format(navdata.gps),
+            text = data_content.replace(/[\']+/g,'');
+
 		origin_data_file.write(text + '\n');
 
-		var latAndlongData = util.format(navdata.gps);
-		var latData = latAndlongData.indexOf('latitude');
-        var longData = latAndlongData.indexOf('longitude');
-        var tmpData = latAndlongData.indexOf('elevation');
-        
-        var latString = latAndlongData.substring(latData + 12 , longData - 6);
-        var longString = latAndlongData.substring(longData + 13 , tmpData - 6);
+		let latAndlongData = util.format(navdata.gps),
+            latData = latAndlongData.indexOf('latitude'),
+            longData = latAndlongData.indexOf('longitude'),
+            tmpData = latAndlongData.indexOf('elevation'),
+            latString = latAndlongData.substring(latData + 12 , longData - 6),
+            longString = latAndlongData.substring(longData + 13 , tmpData - 6);
 
-        current_latitude = latString;
-        current_longtitude = longString;
+        current_latitude = 1;
+        current_longtitude = 1;
+
+        parrot._currentLat = current_latitude;
+        parrot._currentLong = current_longtitude;
+
         console.log("\n현재 Parrot 위치 : " + current_latitude + " , " + current_longtitude);
 	}); 
 	// get current gps data
 
-	client.on('navdata',function(navdata){
-		var data = util.format(navdata.magneto);
-		var degreeStart = data.indexOf('fusionUnwrapped');
-		var degreeEnd = data.indexOf('ok');
-		var degree = data.substring(degreeStart + 17 , degreeEnd - 6);
+	parrot.on('navdata',function(navdata){
+		let data = util.format(navdata.magneto),
+            degreeStart = data.indexOf('fusionUnwrapped'),
+            degreeEnd = data.indexOf('ok'),
+            degree = data.substring(degreeStart + 17 , degreeEnd - 6);
 		
         degree = degree > 360 ? degree - 360 : (degree < 0 ? degree + 360 : degree);
 
         current_degree = degree.toFixed(0);
-        console.log("\n현재 Degree : " + current_degree);
+        console.log("\n현재 방위각 : " + current_degree);
 	});
 	// get current degree data
 
-    if (client._hack == true && isHack == false) {
-        var victim = [];
-        var victim = client._hackData;
+    if (parrot._hack == true && isHack == false) {
+        let victim = parrot._hackData;
 
         dest_distance = victim[0];
         dest_degree = victim[1];
 
-        dest_latitude = client._destLat;
-        dest_longtitude = client._destLong;
+        dest_latitude = parrot._destLat;
+        dest_longtitude = parrot._destLong;
 
         isHack = true;
-
-        console.log("\n목적지 좌표 : " + dest_latitude + " , " + dest_longtitude + "\n목적지 위치 : " + dest_distance + "\n목적지 방위각 : " + dest_degree);
+        console.log("\n목적지 좌표 : " + dest_latitude + " , " + dest_longtitude + "\n목적지까지 거리 : " + dest_distance + " m\n목적지 방위각 : " + dest_degree);
     }
 
 	setTimeout(function(){
-		client.removeAllListeners()
+		parrot.removeAllListeners()
 	},1000);
-	// gps information log stop
 	
 },1000);
--- Hacking_Drone_Module/lib/Client.js
+++ Hacking_Drone_Module/lib/Client.js
@@ -32,12 +32,123 @@ function Client(options) {
   this._lastAltitude      = 0;
 
   // ------------------------ 추가 속성 Start ------------------------ //
+  this._currentLat = 0; // 현재 위도
+  this._currentLong = 0; // 현재 경도
+
   this._destLat = 0; // 목적지 위도
   this._destLong = 0; // 목적지 경도
+
   this._hack = false; // 해당 목적지로 가고있는지 여부
   this._hackData = []; // 해당 목적지 좌표 배열
   // ------------------------ 추가 속성 End ------------------------ //
 }
+
+// ------------------------ 추가 함수 Start ------------------------ //
+Client.prototype.hack = function(dest_lat,dest_lon) {
+
+  function deg2rad(deg) {
+    return (deg * Math.PI / 180);
+  }
+
+  function rad2deg(rad) {
+    return (rad * 180 / Math.PI);
+  }
+
+  function calDistance(P1_latitude, P1_longitude, P2_latitude, P2_longitude) {
+    if ((P1_latitude == P2_latitude) && (P1_longitude == P2_longitude)) return 0;
+
+    var e10 = P1_latitude * Math.PI / 180;
+    var e11 = P1_longitude * Math.PI / 180;
+    var e12 = P2_latitude * Math.PI / 180;
+    var e13 = P2_longitude * Math.PI / 180;
+    /* 타원체 GRS80 */
+    var c16 = 6356752.314140910;
+    var c15 = 6378137.000000000;
+    var c17 = 0.0033528107;
+    var f15 = c17 + c17 * c17;
+    var f16 = f15 / 2;
+    var f17 = c17 * c17 / 2;
+    var f18 = c17 * c17 / 8;
+    var f19 = c17 * c17 / 16;
+    var c18 = e13 - e11;
+    var c20 = (1 - c17) * Math.tan(e10);
+    var c21 = Math.atan(c20);
+    var c22 = Math.sin(c21);
+    var c23 = Math.cos(c21);
+    var c24 = (1 - c17) * Math.tan(e12);
+    var c25 = Math.atan(c24);
+    var c26 = Math.sin(c25);
+    var c27 = Math.cos(c25);
+    var c29 = c18;
+    var c31 = (c27 * Math.sin(c29) * c27 * Math.sin(c29)) + (c23 * c26 - c22 * c27 * Math.cos(c29)) * (c23 * c26 - c22 * c27 * Math.cos(c29));
+    var c33 = (c22 * c26) + (c23 * c27 * Math.cos(c29));
+    var c35 = Math.sqrt(c31) / c33;
+    var c36 = Math.atan(c35);
+    var c38 = 0;
+    var c40 = 0;
+    
+    if (c31 == 0) {
+        c38 = 0;
+    } 
+    else {
+        c38 = c23 * c27 * Math.sin(c29) / Math.sqrt(c31);
+    }
+
+    if ((Math.cos(Math.asin(c38)) * Math.cos(Math.asin(c38))) == 0) {
+        c40 = 0;
+    }
+
+    else {
+        c40 = c33 - 2 * c22 * c26 / (Math.cos(Math.asin(c38)) * Math.cos(Math.asin(c38)));
+    }
+
+    var c41 = Math.cos(Math.asin(c38)) * Math.cos(Math.asin(c38)) * (c15 * c15 - c16 * c16) / (c16 * c16);
+    var c43 = 1 + c41 / 16384 * (4096 + c41 * (-768 + c41 * (320 - 175 * c41)));
+    var c45 = c41 / 1024 * (256 + c41 * (-128 + c41 * (74 - 47 * c41)));
+    var c47 = c45 * Math.sqrt(c31) * (c40 + c45/ 4 * (c33 * (-1 + 2 * c40 * c40) - c45 / 6 * c40 * (-3 + 4 * c31) * (-3 + 4 * c40 * c40)));
+    var c50 = c17 / 16 * Math.cos(Math.asin(c38)) * Math.cos(Math.asin(c38)) * (4 + c17 * (4 - 3 * Math.cos(Math.asin(c38)) * Math.cos(Math.asin(c38))));
+    
+    var c52 = c18 + (1 - c50) * c17 * c38 * (Math.acos(c33) + c50 * Math.sin(Math.acos(c33)) * (c40 + c50 * c33 * (-1 + 2 * c40 * c40)));
+    var c54 = c16 * c43 * (Math.atan(c35) - c47);
+    
+    return c54;
+  }
+
+  function calBearingAngle(P1_latitude, P1_longitude, P2_latitude, P2_longitude) {
+      var current_lat_radian = deg2rad(P1_latitude),
+          current_lon_radian = deg2rad(P1_longitude),
+          destination_lat_radian = deg2rad(P2_latitude),
+          destination_lon_radian =deg2rad(P2_longitude);
+
+      var radian_distance_tmp1 = Math.acos(current_lat_radian) * Math.sin(destination_lat_radian),
+          radian_distance_tmp2 = Math.cos(current_lat_radian) * Math.cos(destination_lat_radian) * Math.cos(current_lon_radian - destination_lon_radian),
+          radian_distance = radian_distance_tmp1 + radian_distance_tmp2;
+
+      var radian_bearing_tmp1 = Math.sin(destination_lat_radian) - Math.sin(current_lat_radian) * Math.cos(radian_distance),
+          radian_bearing_tmp2 = Math.cos(current_lat_radian) * Math.sin(radian_distance),
+          radian_bearing = Math.acos(radian_bearing_tmp1 / radian_bearing_tmp2);
+          // 현재 좌표부터 목적지로의 이동 방향
+
+      var true_bearing = rad2deg(radian_bearing),
+          true_bearing = (Math.sin(destination_lon_radian - current_lon_radian) < 0 ? 360 - true_bearing : true_bearing);
+          
+      return true_bearing.toFixed(2);
+  }
+
+  this._destLat = dest_lat;
+  this._destLong = dest_lon;
+
+  var distance = calDistance(this._currentLat,this._currentLong,this._destLat,this._destLong),
+      degree = calBearingAngle(this._currentLat,this._currentLong,this._destLat,this._destLong)*1,
+      degree = degree > 360 ? degree - 360 : (degree < 0 ? degree + 360 : degree),
+      data = [distance,degree];
+
+  this._hack = true;
+  this._hackData = data;
+  
+  return "Hacking Success!!";
+}
+// ------------------------ 추가 함수 End ------------------------ //
 
 Client.prototype.after = function(duration, fn) {
   setTimeout(fn.bind(this), this._afterOffset + duration);
@@ -276,120 +387,6 @@ Client.prototype.battery = function() {
 Client.prototype._repeat = function(times, fn) {
   this._repeaters.push({times: times, method: fn});
 };
-
-// ------------------------ 추가 함수 Start ------------------------ //
-
-Client.prototype.hack = function(current_lat,current_lon,dest_lat,dest_lon) {
-  console.log("HACKING!!");
-
-  function deg2rad(deg) {
-    return (deg * Math.PI / 180);
-  }
-
-  function rad2deg(rad) {
-    return (rad * 180 / Math.PI);
-  }
-
-  function calDistance(P1_latitude, P1_longitude, P2_latitude, P2_longitude) {
-    if ((P1_latitude == P2_latitude) && (P1_longitude == P2_longitude)) return 0;
-
-    var e10 = P1_latitude * Math.PI / 180;
-    var e11 = P1_longitude * Math.PI / 180;
-    var e12 = P2_latitude * Math.PI / 180;
-    var e13 = P2_longitude * Math.PI / 180;
-    /* 타원체 GRS80 */
-    var c16 = 6356752.314140910;
-    var c15 = 6378137.000000000;
-    var c17 = 0.0033528107;
-    var f15 = c17 + c17 * c17;
-    var f16 = f15 / 2;
-    var f17 = c17 * c17 / 2;
-    var f18 = c17 * c17 / 8;
-    var f19 = c17 * c17 / 16;
-    var c18 = e13 - e11;
-    var c20 = (1 - c17) * Math.tan(e10);
-    var c21 = Math.atan(c20);
-    var c22 = Math.sin(c21);
-    var c23 = Math.cos(c21);
-    var c24 = (1 - c17) * Math.tan(e12);
-    var c25 = Math.atan(c24);
-    var c26 = Math.sin(c25);
-    var c27 = Math.cos(c25);
-    var c29 = c18;
-    var c31 = (c27 * Math.sin(c29) * c27 * Math.sin(c29)) + (c23 * c26 - c22 * c27 * Math.cos(c29)) * (c23 * c26 - c22 * c27 * Math.cos(c29));
-    var c33 = (c22 * c26) + (c23 * c27 * Math.cos(c29));
-    var c35 = Math.sqrt(c31) / c33;
-    var c36 = Math.atan(c35);
-    var c38 = 0;
-    var c40 = 0;
-    
-    if (c31 == 0) {
-        c38 = 0;
-    } 
-    else {
-        c38 = c23 * c27 * Math.sin(c29) / Math.sqrt(c31);
-    }
-
-    if ((Math.cos(Math.asin(c38)) * Math.cos(Math.asin(c38))) == 0) {
-        c40 = 0;
-    }
-
-    else {
-        c40 = c33 - 2 * c22 * c26 / (Math.cos(Math.asin(c38)) * Math.cos(Math.asin(c38)));
-    }
-
-    var c41 = Math.cos(Math.asin(c38)) * Math.cos(Math.asin(c38)) * (c15 * c15 - c16 * c16) / (c16 * c16);
-    var c43 = 1 + c41 / 16384 * (4096 + c41 * (-768 + c41 * (320 - 175 * c41)));
-    var c45 = c41 / 1024 * (256 + c41 * (-128 + c41 * (74 - 47 * c41)));
-    var c47 = c45 * Math.sqrt(c31) * (c40 + c45/ 4 * (c33 * (-1 + 2 * c40 * c40) - c45 / 6 * c40 * (-3 + 4 * c31) * (-3 + 4 * c40 * c40)));
-    var c50 = c17 / 16 * Math.cos(Math.asin(c38)) * Math.cos(Math.asin(c38)) * (4 + c17 * (4 - 3 * Math.cos(Math.asin(c38)) * Math.cos(Math.asin(c38))));
-    
-    var c52 = c18 + (1 - c50) * c17 * c38 * (Math.acos(c33) + c50 * Math.sin(Math.acos(c33)) * (c40 + c50 * c33 * (-1 + 2 * c40 * c40)));
-    var c54 = c16 * c43 * (Math.atan(c35) - c47);
-    
-    return c54;
-  }
-
-  function calBearingAngle(lat1, lon1, lat2, lon2) {
-      var current_lat_radian = deg2rad(lat1);
-      var current_lon_radian = deg2rad(lon1);
-
-      var destination_lat_radian = deg2rad(lat2);
-      var destination_lon_radian =deg2rad(lon2);
-
-      var radian_distance_tmp1 = Math.acos(current_lat_radian) * Math.sin(destination_lat_radian);
-      var radian_distance_tmp2 = Math.cos(current_lat_radian) * Math.cos(destination_lat_radian) * Math.cos(current_lon_radian - destination_lon_radian);
-      var radian_distance = radian_distance_tmp1 + radian_distance_tmp2;
-
-      var radian_bearing_tmp1 = Math.sin(destination_lat_radian) - Math.sin(current_lat_radian) * Math.cos(radian_distance);
-      var radian_bearing_tmp2 = Math.cos(current_lat_radian) * Math.sin(radian_distance) ;
-      var radian_bearing = Math.acos(radian_bearing_tmp1 / radian_bearing_tmp2);
-      // 현재 좌표부터 목적지로의 이동 방향
-
-      var true_bearing = 0;
-      true_bearing = rad2deg(radian_bearing);
-      if (Math.sin(destination_lon_radian - current_lon_radian) < 0) {
-          true_bearing = 360 - true_bearing;
-      }
-      return true_bearing.toFixed(2);
-  }
-
-  var distance = calDistance(current_lat,current_lon,dest_lat,dest_lon);
-  var degree = calBearingAngle(current_lat,current_lon,dest_lat,dest_lon)*1;
-
-  var data = [distance,degree];
-
-  var self = this;
-
-  self._hack = true;
-  self._hackData = data;
-  self._destLat = dest_lat;
-  self._destLong = dest_lon;
-  
-  return "Hacking Success!!";
-}
-
-// ------------------------ 추가 함수 End ------------------------ //
 
 var pcmdOptions = [
   ['up', 'down'],
